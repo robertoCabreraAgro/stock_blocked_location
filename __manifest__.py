d7a4d4de6630d16f93a6d6a2954db82917d5ec74
robertoCabreraAgro <roberto.cabrera@agromarin.mx>
2025-05-12 10:25:51 -0600

    update

--- __manifest__.py
+++ __manifest__.py
@@ -1,29 +1,29 @@
 {
-    'name': 'Block Location',
-    'version': 'saas~18.2.0.0.1',
-    'summary': 'Block outgoing operations from specific stock locations',
-    'category': 'Warehouse',
-    'author': 'Robert',
-    'website': 'https://www.agromarin.mx',
-    'license': 'AGPL-3',
-    'depends': [
-        'base',
-        'stock',
-        'mail',
-        'point_of_sale',
+    "name": "Block Location",
+    "version": "saas~18.2.0.0.1",
+    "summary": "Block outgoing operations from specific stock locations",
+    "category": "Warehouse",
+    "author": "Robert",
+    "website": "https://www.agromarin.mx",
+    "license": "AGPL-3",
+    "depends": [
+        "base",
+        "stock",
+        "mail",
+        "point_of_sale",
     ],
-    'data': [
-        'security/security.xml',
-        'security/ir.model.access.csv',
-        'views/stock_location_view.xml',
+    "data": [
+        "security/security.xml",
+        "security/ir.model.access.csv",
+        "views/stock_location_view.xml",
     ],
-    'assets': {
-        'point_of_sale.assets': [
-            'stock_blocked_location/static/src/js/pos.js',
-            'stock_blocked_location/static/src/xml/pos.xml',
-            'stock_blocked_location/static/src/css/pos.css',
+    "assets": {
+        "point_of_sale._assets_pos": [
+            "stock_blocked_location/static/src/js/pos.js",
+            "stock_blocked_location/static/src/xml/pos.xml",
+            "stock_blocked_location/static/src/css/pos.css",
         ],
     },
-    'installable': True,
-    'application': False,
+    "installable": True,
+    "application": False,
 }
--- models/pos_config.py
+++ models/pos_config.py
@@ -1,12 +1,11 @@
 from odoo import models
 
-
 class PosConfig(models.Model):
     _inherit = 'pos.config'
 
     def get_display_stock_pos(self):
         get_param = self.env['ir.config_parameter'].sudo().get_param
         return [
-            get_param('pos_stock_display.display_stock_pos'),
-            get_param('pos_stock_display.hide_out_of_stock_product'),
+            get_param('pos_stock_display.display_stock_pos', 'False'),
+            get_param('pos_stock_display.hide_out_of_stock_product', 'False'),
         ]
