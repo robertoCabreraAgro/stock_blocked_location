3354e0032e944b51ae3e9367e0f77d65b02eea6f
robertoCabreraAgro <roberto.cabrera@agromarin.mx>
2025-05-12 13:41:26 -0600

    update

--- __manifest__.py
+++ __manifest__.py
@@ -1,29 +1,52 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU AFFERO
+#    GENERAL PUBLIC LICENSE (AGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU AFFERO GENERAL PUBLIC LICENSE (AGPL v3) for more details.
+#
+#    You should have received a copy of the GNU AFFERO GENERAL PUBLIC LICENSE
+#    (AGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
 {
-    "name": "Block Location",
+    'name': 'POS Product Stock',
     "version": "saas~18.2.0.0.1",
-    "summary": "Block outgoing operations from specific stock locations",
-    "category": "Warehouse",
-    "author": "Robert",
-    "website": "https://www.agromarin.mx",
-    "license": "AGPL-3",
-    "depends": [
-        "base",
-        "stock",
-        "mail",
-        "point_of_sale",
+    'category': 'Point Of Sale',
+    'summary': "Quantity of  all Products in each Warehouse",
+    'description': "Shows Stock quantity in POS  for all Products in each Warehouse, Odoo 18",
+    'author': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'company': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['base','point_of_sale','stock'],
+    'data': [
+        'views/res_cofig_settings_views.xml',
+        'views/product_template_views.xml',
     ],
-    "data": [
-        "security/security.xml",
-        "security/ir.model.access.csv",
-        "views/stock_location_view.xml",
-    ],
-    "assets": {
-        "point_of_sale._assets_pos": [
-            "stock_blocked_location/static/src/js/pos.js",
-            "stock_blocked_location/static/src/xml/pos.xml",
-            "stock_blocked_location/static/src/css/pos.css",
+    'assets': {
+        'point_of_sale._assets_pos': [
+            'pos_product_stock/static/src/xml/product_item.xml',
+            'pos_product_stock/static/src/css/product_quantity.scss',
+            'pos_product_stock/static/src/js/pos_location.js',
+            'pos_product_stock/static/src/js/pos_payment_screen.js',
+            'pos_product_stock/static/src/js/pos_session.js',
+            'pos_product_stock/static/src/js/deny_order.js',
         ],
     },
-    "installable": True,
-    "application": False,
+    'images': ['static/description/banner.jpg'],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
 }
--- models/pos_session.py
+++ models/pos_session.py
@@ -19,9 +19,16 @@
 #    If not, see <http://www.gnu.org/licenses/>.
 #
 #############################################################################
-from . import pos_config
-from . import pos_session
-from . import product_template
-from . import res_config_settings # remove this
-from . import stock_move_line
-from . import stock_quant
+from odoo import api, models
+
+
+class PosSession(models.Model):
+    """inherit pos.session to add fields and modules in session."""
+    _inherit = 'pos.session'
+
+    @api.model
+    def _load_pos_data_models(self, config_id):
+        """The list of models to be loaded for POS data."""
+        data = super()._load_pos_data_models(config_id)
+        data += ['res.config.settings','stock.quant','stock.move.line']
+        return data
